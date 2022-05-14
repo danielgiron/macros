611198c2b4942e94396269a45a6b302a3f371b23
danielgiron <73446404+danielgiron@users.noreply.github.com>
2022-05-14 14:47:27 -0400

    added mesh gradient background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import logo from "./logo.svg";
 import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+
+import { setValues, changeBG } from "./background";
 
 import "./App.css";
 import Header from "./Header";
 import Main from "./Main";
 
 function App() {
+  useEffect(() => {
+    console.log("loaded");
+    const elem = document.querySelector(".colorMesh");
+    elem.style.backgroundColor = "#e7a247";
+    const points = setValues();
+
+    setInterval(() => {
+      changeBG(elem, points);
+    }, 50);
+  }, []);
+
   return (
     <div className="App">
       <Header />
 
       <Main />
+      <div className="colorMesh"></div>
+      <script src="./background.js"></script>
     </div>
   );
 }
